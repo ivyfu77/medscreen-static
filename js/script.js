fa4d9e5af7980a39425699b0fe0781700f72a07d
Ivy Fu <ivy@hmsnz.com>
2017-04-05 17:45:32 +1200

    Add narrow-screen.css define styles when screen less than 850px (Toggle menu)

--- js/script.js
+++ js/script.js
@@ -31,4 +31,15 @@ $(document).ready(function() {
 	    }
 	  });
 	});
+
 });
+var isClicked = false;
+function toggleMenu() {
+	isClicked = !isClicked;
+	var obj = document.getElementById("popup");
+	if (isClicked) {
+	  obj.className += "open";
+	} else {
+	  obj.className = "";
+	}
+}
